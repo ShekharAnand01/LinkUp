a184718fae0d37e3bf29ff9176c9e488d68135f6
Shekhar Anand <shekharanand134@gmail.com>
2024-08-30 10:22:48 +0530

    Fragment transaction to move to different fragments added

--- app/src/main/java/com/example/linkup/MainActivity.kt
+++ app/src/main/java/com/example/linkup/MainActivity.kt
@@ -1,20 +1,68 @@
 package com.example.linkup
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.linkup.databinding.ActivityMainBinding
+import com.example.linkup.fragments.ChatFragment
+import com.example.linkup.fragments.ProfileFragment
+import com.example.linkup.fragments.SearchUserFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val chatFragment = ChatFragment()
+    private val profileFragment = ProfileFragment()
+    private val searchUserFragment = SearchUserFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        binding.mainSearchBtn.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frame_layout, searchUserFragment)
+                .addToBackStack(null)
+                .commit()
+        }
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_chat -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frame_layout, chatFragment)
+                        .commit()
+                    true
+                }
+                R.id.menu_profile -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frame_layout, profileFragment)
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        binding.bottomNavigation.selectedItemId = R.id.menu_chat
+
+
     }
+
 }
